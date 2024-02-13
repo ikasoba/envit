613763a640bcf196d4d6c3d11dc0a86aea8fbc1d
ikasoba <57828948+ikasoba@users.noreply.github.com>
2024-02-14 00:33:13 +0900

    add symbolic copy

--- __envit__/copy.sh
+++ __envit__/copy.sh
@@ -17,6 +17,7 @@ CopyDirectory() {
 
     (BeginRunAnyOne
       Run cp -lf $item $target/$base
+      Run cp -sf $item $target/$base
       Run cp -Lf $item $target/$base
     EndRunAnyOne) > /dev/null
   done
